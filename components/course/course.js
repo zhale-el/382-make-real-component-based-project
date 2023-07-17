7ec80664270d55dd4c50c93b3a7b14af319bec00
zhaleh <zhalehelmi@gmail.com>
2023-07-17 16:43:33 +0330

    completed-component-course

--- components/course/course.js
+++ components/course/course.js
@@ -2,6 +2,22 @@ const template = document.createElement("template");
 template.innerHTML = `
 
 <link rel="stylesheet" href="../components/course/course.css">
+<div class="course-card">
+<div class="cover">
+  <img src="./img/js.png" alt="" />
+</div>
+<div class="details">
+  <h2>js course</h2>
+  <div class="info">
+    <p>students:3000</p>
+    <p>teacher:zhale</p>
+  </div>
+  <div class="actions">
+    <button id="register">register</button>
+    <button id="toggle">show Details</button>
+  </div>
+</div>
+</div>
 `;
 
 class course extends HTMLElement {
